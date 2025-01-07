40271a4ac900a5a1f5cf94420eaa6f495761d3a6
vedantsinggh <vedantsinggh@gmail.com>
2025-01-07 23:50:20 +0530

    fixed import error in browser

--- index.js
+++ index.js
@@ -2,6 +2,3 @@ import Widget from "./flux.js";
 
 const widget = new Widget("div", {});
 console.log(widget);
-const entry = document.getElementById("entry");
-entry.appendChild(widget.createTag());
-
